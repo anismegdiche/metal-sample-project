ab993497c756c89330fb66cfbdf1b0d087994baa
Anis Megdiche <anis.megdiche@gmail.com>
2023-11-10 19:16:36 +0100

    init

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-#
-#   Prepare
-#
-DOCKER_CMD="docker-compose"
 
 #
 #   Mongodb
 #
 
 chmod +x ./sample/mongodb/sample_mflix/import.sh
-$DOCKER_CMD up -d mongodb
+docker-compose up -d mongodb
 sleep 20
-$DOCKER_CMD exec mongodb /mongodb-samples/import.sh
-$DOCKER_CMD down
+docker-compose exec mongodb /mongodb-samples/import.sh
+docker-compose down
 
 #
 #   MS SQL Server
@@ -21,10 +17,10 @@ mkdir -p mssql
 chmod 777 mssql
 chmod +x ./sample/mssql/import.sh
 
-$DOCKER_CMD up -d mssql
-sleep 20
-$DOCKER_CMD exec mssql /mssql-samples/import.sh
-$DOCKER_CMD down
+docker-compose up -d mssql
+sleep 30
+docker-compose exec mssql /mssql-samples/import.sh
+docker-compose down
 
 #
 #   Postgres 1
@@ -33,10 +29,10 @@ mkdir -p postgres1
 chown 999:0 postgres1
 chmod +x ./sample/postgres1/import.sh
 
-$DOCKER_CMD up -d postgres1
+docker-compose up -d postgres1
 sleep 20
-$DOCKER_CMD exec postgres1 /postgres1-samples/import.sh
-$DOCKER_CMD down
+docker-compose exec postgres1 /postgres1-samples/import.sh
+docker-compose down
 
 #
 #   Postgres 2
@@ -45,7 +41,7 @@ mkdir -p postgres2
 chown 999:0 postgres2
 chmod +x ./sample/postgres2/import.sh
 
-$DOCKER_CMD up -d postgres2
+docker-compose up -d postgres2
 sleep 20
-$DOCKER_CMD exec postgres2 /postgres2-samples/import.sh
-$DOCKER_CMD down
+docker-compose exec postgres2 /postgres2-samples/import.sh
+docker-compose down
