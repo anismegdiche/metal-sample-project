0ae565d5f1790c1735bbb13ea9e9c325f2b1137c
Anis Megdiche <anis.megdiche@gmail.com>
2023-11-18 20:22:53 +0100

    init

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ chmod +x ./sample/mongodb/sample_mflix/import.sh
 docker-compose up -d mongodb
 sleep 20
 docker-compose exec mongodb /mongodb-samples/import.sh
-docker-compose down
 
 #
 #   MS SQL Server
@@ -25,7 +24,6 @@ chmod +x ./sample/mssql/import.sh
 docker-compose up -d mssql
 sleep 30
 docker-compose exec mssql /mssql-samples/import.sh
-docker-compose down
 
 #
 #   Postgres 1
@@ -37,7 +35,6 @@ chmod +x ./sample/postgres1/import.sh
 docker-compose up -d postgres1
 sleep 20
 docker-compose exec postgres1 /postgres1-samples/import.sh
-docker-compose down
 
 #
 #   Postgres 2
@@ -49,7 +46,6 @@ chmod +x ./sample/postgres2/import.sh
 docker-compose up -d postgres2
 sleep 20
 docker-compose exec postgres2 /postgres2-samples/import.sh
-docker-compose down
 
 # Start the stack
 docker-compose up -d
